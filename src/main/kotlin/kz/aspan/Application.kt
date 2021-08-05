ca1c03940ed4854b7a5320b54b1202699ccd731d
Samgar Tilek <aspansdu@gmail.com>
2021-08-05 22:58:19 +0600

    Setting up MongoDB

--- src/main/kotlin/kz/aspan/Application.kt
+++ src/main/kotlin/kz/aspan/Application.kt
@@ -1,11 +1,28 @@
 package kz.aspan
 
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.routing.Routing
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import kz.aspan.plugins.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kz.aspan.data.collections.User
+import kz.aspan.data.registerUser
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
+        install(DefaultHeaders)
+        install(CallLogging)
+        install(Routing)
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
     }.start(wait = true)
 }
---
+++ src/main/kotlin/kz/aspan/data/NotesDatabase.kt
@@ -0,0 +1,15 @@
+package kz.aspan.data
+
+import kz.aspan.data.collections.Note
+import kz.aspan.data.collections.User
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+private val client = KMongo.createClient().coroutine
+private val database = client.getDatabase("NotesDatabase")
+private val users = database.getCollection<User>()
+private val notes = database.getCollection<Note>()
+
+suspend fun registerUser(user: User): Boolean {
+    return users.insertOne(user).wasAcknowledged()
+}
