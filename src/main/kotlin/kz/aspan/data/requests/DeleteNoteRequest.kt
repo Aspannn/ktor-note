d376e76f8c755768330b90fefa9d5469fabd5471
Samgar Tilek <aspansdu@gmail.com>
2021-08-07 19:20:53 +0600

    Save,Delete,Update notes

---
+++ src/main/kotlin/kz/aspan/data/requests/DeleteNoteRequest.kt
@@ -0,0 +1,5 @@
+package kz.aspan.data.requests
+
+data class DeleteNoteRequest(
+    val id: String
+)
--- src/main/kotlin/kz/aspan/routes/NoteRoutes.kt
+++ src/main/kotlin/kz/aspan/routes/NoteRoutes.kt
@@ -4,12 +4,20 @@ import io.ktor.application.call
 import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.authenticate
 import io.ktor.auth.principal
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
+import kz.aspan.data.collections.Note
+import kz.aspan.data.deleteNoteForUser
 import kz.aspan.data.getNotesForUser
+import kz.aspan.data.requests.DeleteNoteRequest
+import kz.aspan.data.saveNotes
 
 fun Route.noteRoutes() {
     route("/getNotes") {
@@ -18,6 +26,45 @@ fun Route.noteRoutes() {
                 val email = call.principal<UserIdPrincipal>()!!.name
                 val notes = getNotesForUser(email)
                 call.respond(OK, notes)
+            }
+        }
+    }
+
+    route("/deleteNote") {
+        authenticate {
+            post {
+                val email = call.principal<UserIdPrincipal>()!!.name
+                val request = try {
+                    call.receive<DeleteNoteRequest>()
+                } catch (e: ConcurrentModificationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+
+                if (deleteNoteForUser(email, request.id)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
+            }
+        }
+    }
+
+    route("/addNote") {
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                } catch (e: ConcurrentModificationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+
+                if (saveNotes(note)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
             }
         }
     }
